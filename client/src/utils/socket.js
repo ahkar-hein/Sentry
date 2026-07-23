80bfeb84f56c2e4ef9733d5b0c84fab50595c615
Ahkar Hein <ahkarhein04@gmail.com>
2026-07-23 16:05:08 -0700

    add environment variables for deployment

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Single shared socket instance
-const socket = io("http://localhost:5000", { autoConnect: false });
+const socket = io(import.meta.env.VITE_SERVER_URL || "http://localhost:5000", { autoConnect: false });
 
 export default socket;
